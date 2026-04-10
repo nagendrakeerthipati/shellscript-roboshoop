351c3a2eb3ac0347bfc08b5b69dc6a6db77ce56a
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2026-04-10 12:53:43 +0200

    practice started

--- mongodb.sh
+++ mongodb.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-if [ $USERID -ne o]; then
-echo "Please run this script with root access"
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]; then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
- echo "you are running with root access"
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-#install MongoDB
-dnf install mongodb-org -y 
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
 
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
+VALIDATE $? "Copying MongoDB repo"
 
-# Start & Enable MongoDB Service
-systemctl start mongod
-systemctl status mongod
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb server"
 
-#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB"
 
-sed -i 's/12.0.0.1/0.0.0.0/g' /etc/mongod.conf
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting MongoDB"
 
-#restart the service 
-systemctl restart mongod
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Editing MongoDB conf file for remote connections"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB"
